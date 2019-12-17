35b45d8f5e0a27080e3c3a3699f08afad88a8777
Caleb JA <skywise@tutanota.de>
2019-12-17 12:48:18 -0500

    Remove unused functions

--- web/template/template.php
+++ web/template/template.php
@@ -3,13 +3,9 @@
 
 <head>
   <meta charset="utf-8" />
-  <title><?php page_title(); ?> | <?php site_name(); ?></title>
+  <title><?php site_name(); ?></title>
   <link href="data:image/gif;base64,R0lGODlhEAAQAPIFAIANDcQTE+4+Pv+8APitrf///wAAAAAAACH5BAUKAAUALAAAAAAQABAAAANHWLrcFcHJB8FscEQKIeteQIxEp1QAAAksyVLWI4xsK3AyScw1F8w64K6gEohIF9XK+NkQA0pQDKpIRUGgKjVV6cS4PudlnAAAOw==" rel="icon">
-  <style>
-    body {
-      display: none;
-    }
-  </style>
+  <style>body{display:none}</style>
   <link href="/static/style.css" rel="stylesheet" type="text/css" />
 </head>
 
@@ -23,6 +19,7 @@
   </main>
 
   <footer>
+    <p>Reload the page to generate a new character.</p>
     <p>Based on the <a href="https://perchance.org/wofocgen-testversion">WoF OC Generator</a> hosted on perchance.org.</p>
     <p>Written in PHP. <a href="https://git.sr.ht/~wychmire/wofocgen">Opensource</a> and available under the ISC license.</p>
     <p>Created by Wychmire - Hosted by Heroku</p>
