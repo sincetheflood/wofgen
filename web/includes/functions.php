236fb1e331965c56343f02095e9ad21f5a560572
Caleb JA <skywise@tutanota.de>
2019-12-17 16:23:06 -0500

    Refactor to use snake_case in proper PHP fashion

--- web/includes/functions.php
+++ web/includes/functions.php
@@ -17,6 +17,13 @@ function site_version()
 }
 
 /**
+ * Chooses a random number between 1 and X.
+ */
+function roll ($sides) {
+  return mt_rand(1,$sides);
+}
+
+/**
  * Starts everything and displays the template.
  */
 function init()
--- web/includes/generator.php
+++ web/includes/generator.php
@@ -5,15 +5,23 @@
 \*------------------------------------*/
 
 /**
- * $Tribes
- * Read ./resources/tribe/*, ./resources/isHybrid and randomize entries.
+ * $Random Numbers
+ * Choose two random numbers between 1 and 100 for determinig if the dragon is
+ * a hybrid and/or nobility
  */
-$tribePantala = explode("\n", file_get_contents('./resources/tribe/pantala'));
-$tribePyrrhia = explode("\n", file_get_contents('./resources/tribe/pyrrhia'));
-$isHybrid     = explode("\n", file_get_contents('./resources/isHybrid'));
-$tribeOptions = array_merge($tribePantala, $tribePyrrhia);
-shuffle($isHybrid);
-shuffle($tribeOptions);
+$nobility_chance = roll(100);
+$hybrid_chance = roll(100);
+
+
+
+/**
+ * $Tribes
+ * Read ./resources/tribe/*, ./resources/is_hybrid and randomize entries.
+ */
+$tribe_pantala = explode("\n", file_get_contents('./resources/tribe/pantala'));
+$tribe_pyrrhia = explode("\n", file_get_contents('./resources/tribe/pyrrhia'));
+$tribe_options = array_merge($tribe_pantala, $tribe_pyrrhia);
+shuffle($tribe_options);
 
 
 
@@ -22,17 +30,19 @@ shuffle($tribeOptions);
  * Read ./resources/body/* and randomize entries.
  */
 
+// Rank specific horn attributes
+$body_description_nobility = explode("\n", file_get_contents('./resources/body/description_nobility'));
 // Tribe specific body attributes
-$bodyDescriptionSea = explode("\n", file_get_contents('./resources/body/descriptionSea'));
+$body_description_sea = explode("\n", file_get_contents('./resources/body/description_sea'));
 // Shared body attributes
-$bodyAdjectiveShared = explode("\n", file_get_contents('./resources/body/adjectiveShared'));
-$bodyScaleShared = explode("\n", file_get_contents('./resources/body/scaleShared'));
-$bodyDescriptionShared = explode("\n", file_get_contents('./resources/body/descriptionShared'));
+$body_adjective_shared   = explode("\n", file_get_contents('./resources/body/adjective_shared'));
+$body_scale_shared       = explode("\n", file_get_contents('./resources/body/scale_shared'));
+$body_description_shared = explode("\n", file_get_contents('./resources/body/description_shared'));
 // Randomize body attributes
-shuffle($bodyAdjectiveShared);
-shuffle($bodyScaleShared);
-shuffle($bodyDescriptionSea);
-shuffle($bodyDescriptionShared);
+shuffle($body_adjective_shared);
+shuffle($body_scale_shared);
+shuffle($body_description_sea);
+shuffle($body_description_shared);
 
 
 
@@ -42,55 +52,54 @@ shuffle($bodyDescriptionShared);
  */
 
 // Tribe specific wing attributes
-$wingSizeHybrid = explode("\n", file_get_contents('./resources/wing/sizeHybrid'));
+$wing_size_hybrid = explode("\n", file_get_contents('./resources/wing/size_hybrid'));
 // Shared wing attributes
-$wingAppearanceShared = explode("\n", file_get_contents('./resources/wing/appearanceShared'));
-$wingColorShared = explode("\n", file_get_contents('./resources/wing/colorShared'));
-$wingSizeShared = explode("\n", file_get_contents('./resources/wing/sizeShared'));
+$wing_appearance_shared = explode("\n", file_get_contents('./resources/wing/appearance_shared'));
+$wing_color_shared      = explode("\n", file_get_contents('./resources/wing/color_shared'));
+$wing_size_shared       = explode("\n", file_get_contents('./resources/wing/size_shared'));
 // Randomize wing attributes
-shuffle($wingSizeHybrid);
-shuffle($wingAppearanceShared);
-shuffle($wingColorShared);
-shuffle($wingSizeShared);
+shuffle($wing_size_hybrid);
+shuffle($wing_appearance_shared);
+shuffle($wing_color_shared);
+shuffle($wing_size_shared);
 
 
 
 /**
- * $Horns
+ * $_horns
  * Read ./resources/horn/* and randomize entries.
  */
 
-// Tribe specific horn attributes
-// $hornSea = explode("\n", file_get_contents('./resources/horn/sea'));
-$hornSea;
+// Rank specific horn attributes
+$horn_appearance_nobility = explode("\n", file_get_contents('./resources/horn/appearance_nobility'));
+$horn_size_nobility       = explode("\n", file_get_contents('./resources/horn/size_nobility'));
 // Shared horn attributes
-$hornAppearanceShared = explode("\n", file_get_contents('./resources/horn/appearanceShared'));
-$hornSizeShared = explode("\n", file_get_contents('./resources/horn/sizeShared'));
+$horn_appearance_shared = explode("\n", file_get_contents('./resources/horn/appearance_shared'));
+$horn_size_shared       = explode("\n", file_get_contents('./resources/horn/size_shared'));
 // Randomize horn attributes
-// shuffle($hornSea);
-shuffle($hornAppearanceShared);
-shuffle($hornSizeShared);
+shuffle($horn_appearance_shared);
+shuffle($horn_size_shared);
 
 
 
 /**
- * $Locations
+ * $_locations
  * Read ./resources/location/* and randomize the entries.
  */
 
 // Tribe specific locations
-$locationHiveSilk = explode("\n", file_get_contents('./resources/location/hiveSilk'));
-$locationIce = explode("\n", file_get_contents('./resources/location/ice'));
-$locationLeaf = explode("\n", file_get_contents('./resources/location/leaf'));
+$location_hive_silk = explode("\n", file_get_contents('./resources/location/hive_silk'));
+$location_ice      = explode("\n", file_get_contents('./resources/location/ice'));
+$location_leaf     = explode("\n", file_get_contents('./resources/location/leaf'));
 // Shared locations
-$locationPantalaShared = explode("\n", file_get_contents('./resources/location/pantalaShared'));
-$locationPyrrhiaShared = explode("\n", file_get_contents('./resources/location/pyrrhiaShared'));
+$location_pantala_shared = explode("\n", file_get_contents('./resources/location/pantala_shared'));
+$location_pyrrhia_shared = explode("\n", file_get_contents('./resources/location/pyrrhia_shared'));
 // Randomize tribe locations
-shuffle($locationHiveSilk);
-shuffle($locationIce);
-shuffle($locationLeaf);
-shuffle($locationPantalaShared);
-shuffle($locationPyrrhiaShared);
+shuffle($location_hive_silk);
+shuffle($location_ice);
+shuffle($location_leaf);
+shuffle($location_pantala_shared);
+shuffle($location_pyrrhia_shared);
 
 
 
@@ -99,10 +108,13 @@ shuffle($locationPyrrhiaShared);
  * Read ./resources/job/* and randomize entries.
  */
 
+// Tribe specific jobs
+// _leafWing incompatable jobs
+$job_remove_leaf = explode("\n", file_get_contents('./resources/job/remove_leaf'));
 // Shared jobs
-$jobShared = explode("\n", file_get_contents('./resources/job/shared'));
+$job_shared = explode("\n", file_get_contents('./resources/job/shared'));
 // Randomize jobs
-shuffle($jobShared);
+shuffle($job_shared);
 
 
 
@@ -112,9 +124,9 @@ shuffle($jobShared);
  */
 
 // Shared hobbies
-$hobbyShared = explode("\n", file_get_contents('./resources/hobby/shared'));
+$hobby_shared = explode("\n", file_get_contents('./resources/hobby/shared'));
 // Randomize hobbies
-shuffle($hobbyShared);
+shuffle($hobby_shared);
 
 
 
@@ -123,10 +135,12 @@ shuffle($hobbyShared);
  * Read ./resources/status/* and randomize entries.
  */
 
-// Shared hobbies
-$statusShared = explode("\n", file_get_contents('./resources/status/shared'));
-// Randomize hobbies
-shuffle($statusShared);
+// Shared statuses
+$status_shared  = explode("\n", file_get_contents('./resources/status/shared'));
+$status_pyrrhia = explode("\n", file_get_contents('./resources/status/pyrrhia'));
+// Randomize statuses
+shuffle($status_shared);
+shuffle($status_pyrrhia);
 
 
 
@@ -136,60 +150,116 @@ shuffle($statusShared);
 #COMPUTATIONS
 \*------------------------------------*/
 
-// Determine the dragon's tribe
-if ($isHybrid[0] === "true") {
-  $tribe = "$tribeOptions[0] and $tribeOptions[1] hybrid";
+// Determine the dragon's rank
+if ($nobility_chance >= 80) {
+  global $is_nobility, $body_description_options, $horn_size_options, $horn_appearance_options;
+  $is_nobility = true;
+
+  // Body
+  $body_description_options = array_merge($body_description_shared, $body_description_nobility);
+  shuffle($body_description_options);
+
+  // Horns
+  $horn_size_options       = array_merge($horn_size_nobility, $horn_size_shared);
+  $horn_appearance_options = array_merge($horn_appearance_nobility, $horn_appearance_shared);
+  shuffle($horn_size_options);
+  shuffle($horn_appearance_options);
 } else {
-  $tribe = "$tribeOptions[0]";
+  global $is_nobility, $body_description_options, $horn_size_options, $horn_appearance_options;
+  $is_nobility = false;
+
+  // Body
+  $body_description_options = $body_description_shared;
+
+  // Horns
+  $horn_size_options       = $horn_size_shared;
+  $horn_appearance_options = $horn_appearance_shared;
 }
 
 
-// Determine the dragon's markings based on their tribe.
-if ($tribe === "SeaWing") {
-  global $bodyDescriptionOptions;
-  $bodyDescriptionOptions = array_merge($bodyDescriptionShared, $bodyDescriptionSea);
-  shuffle($bodyDescriptionOptions);
+
+// Determine the dragon's tribe
+if ($hybrid_chance >= 80) {
+  global $is_hybrid, $tribe;
+  $is_hybrid = true;
+
+  $tribe = "$tribe_options[0] and $tribe_options[1] hybrid";
 } else {
-  global $bodyDescriptionOptions;
-  $bodyDescriptionOptions = $bodyDescriptionShared;
+  global $is_hybrid, $tribe;
+  $is_hybrid = false;
+  $tribe = "$tribe_options[0]";
+}
+
+
+
+
+// Determine the dragon's markings based on their tribe.
+if ($tribe === "_seaWing") {
+  global $body_description_options;
+  $body_description_options = array_merge($body_description_shared, $body_description_sea);
+  shuffle($body_description_options);
+} else {
+  global $body_description_options;
+  $body_description_options = $body_description_shared;
 }
 
 
 // Determine the dragon's wing size
-if ($isHybrid[0] === "true") {
-  global $wingSizeOptions;
-  $wingSizeOptions = array_merge($wingSizeHybrid, $wingSizeShared);
-  shuffle($wingSizeOptions);
+if ($is_hybrid === true) {
+  global $wing_size_options;
+  $wing_size_options = array_merge($wing_size_hybrid, $wing_size_shared);
+  shuffle($wing_size_options);
 } else {
-  global $wingSizeOptions;
-  $wingSizeOptions = $wingSizeShared;
+  global $wing_size_options;
+  $wing_size_options = $wing_size_shared;
 }
 
 
 // Determine the dragon's horn appearance
-$hornOptions = $hornAppearanceShared;
+$horn_size_options       = $horn_size_shared;
+$horn_appearance_options = $horn_appearance_shared;
 
 
 // Determine the dragon's job
-$jobOptions = $jobShared;
+if ($tribe === "_leafWing") {
+  global $job_options;
+  // $job_options = \array_diff($job_shared, $job_remove_leaf);
+  $job_options = array_merge(array_diff($job_shared, $job_remove_leaf), array_diff($job_remove_leaf, $job_shared));
+  shuffle($job_options);
+} else {
+  global $job_options;
+  $job_options = $body_description_shared;
+}
 
 
 // Determine the dragon's location based on their tribe.
-if ($tribe === "IceWing") {
-  global $locationOptions;
-  $locationOptions = array_merge($locationPyrrhiaShared, $locationIce);
-  shuffle($locationOptions);
-} else if ($tribe === "HiveWing" || $tribe === "SilkWing") {
-  global $locationOptions;
-  $locationOptions = array_merge($locationPantalaShared, $locationHiveSilk);
-  shuffle($locationOptions);
-} else if ($tribe === "LeafWing") {
-  global $locationOptions;
-  $locationOptions = array_merge($locationPantalaShared, $locationLeaf);
-  shuffle($locationOptions);
+if ($tribe === "_iceWing") {
+  global $location_options;
+  $location_options = array_merge($location_pyrrhia_shared, $location_ice);
+  shuffle($location_options);
+} else if ($tribe === "_hiveWing" || $tribe === "_silkWing") {
+  global $location_options;
+  $location_options = array_merge($location_pantala_shared, $location_hive_silk);
+  shuffle($location_options);
+} else if ($tribe === "_leafWing") {
+  global $location_options;
+  $location_options = array_merge($location_pantala_shared, $location_leaf);
+  shuffle($location_options);
 } else {
-  global $locationOptions;
-  $locationOptions = $locationPyrrhiaShared;
+  global $location_options;
+  $location_options = $location_pyrrhia_shared;
+}
+
+// Determine the dragon's hobby
+$hobby_options = $hobby_shared;
+
+
+// Determine the dragon's status
+if (array_search("$tribe", $tribe_pyrrhia) || $is_hybrid === true) {
+  $status_options = array_merge($status_shared, $status_pyrrhia);
+  shuffle($status_options);
+} else {
+  $status_options = $status_shared;
 }
 
 
@@ -200,6 +270,6 @@ if ($tribe === "IceWing") {
 #PRINT
 \*------------------------------------*/
 
-echo "<p>This dragon is $bodyAdjectiveShared[0] $tribe with $bodyScaleShared[0] scales. Their wings are $wingSizeOptions[0] and $wingAppearanceShared[0], $wingColorShared[0]. They have $bodyDescriptionOptions[0] and $hornSizeShared[0] $hornOptions[0].</p>";
+echo "<p>This dragon is $body_adjective_shared[0] $tribe with $body_scale_shared[0] scales. Their wings are $wing_size_options[0] and $wing_appearance_shared[0], $wing_color_shared[0]. They have $body_description_options[0] and $horn_size_options[0] $horn_appearance_options[0].</p>";
 
-echo "<p>They are a $jobOptions[0] and live $locationOptions[0]. They $hobbyShared[0] and $statusShared[0].</p>";
+echo "<p>They are a $job_options[0] and live $location_options[0]. They $hobby_options[0] and $status_options[0].</p>";
