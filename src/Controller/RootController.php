5f516fbe5bcad7bc41a29b45374d0bd66cf6ec04
Caleb JA <caleb@himmalerin.net>
2021-01-23 19:04:36 -0500

    Switch to manually adding generators the the index file for now.

--- src/Controller/RootController.php
+++ src/Controller/RootController.php
@@ -6,33 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
-use function Symfony\Component\String\u;
 
 class RootController extends AbstractController
 {
     /**
      * @Route("/", name="app_index")
      */
-    public function index(RouterInterface $router): Response
+    public function index(): Response
     {
-        $allRoutes = $router->getRouteCollection()->all();
-
-        $generatorRoutes = [];
-        foreach ($allRoutes as $routeName => $route) {
-            if (strpos($routeName, 'app_generator_') === 0) {
-                // Trim the "app_generator_" off of the name
-                $name = str_replace('app_generator_', '', $routeName);
-                // Then turn the name into a "title" (replace underscores with spaces, capitalize each word)
-                $prettyName = u($name)->replace('_', ' ')->title(true)->toString();
-                $generatorRoutes[$prettyName] = $routeName;
-            }
-        }
-
-
         return $this->render('root/index.html.twig', [
             'controller_name' => 'RootController',
-
-            'generators' => $generatorRoutes,
         ]);
     }
 }
